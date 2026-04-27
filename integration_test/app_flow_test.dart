56784f200090e2c47afdee74fd2efcaf2154f7a0
vibe-coding15 <emailbaruku50@gmail.com>
2026-04-27 22:53:59 +0800

    ci: stabilize integration_test execution without device dependency

--- integration_test/app_flow_test.dart
+++ integration_test/app_flow_test.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('transaction flow smoke test', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: Scaffold(body: Text('FinSage Transaction Flow'))));
